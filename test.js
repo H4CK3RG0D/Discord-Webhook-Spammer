bdc98578531007029f3fdd7192ec801171363be6
Hein Thu <74117599+H4CK3RG0D@users.noreply.github.com>
2022-12-31 19:51:42 -0600

    Update test.jsd

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
-const config = require('./config.json');
-
-// console logs the url2 from config.json
-console.log(config.url[0].url1);
-
-// console logs the username from config.json
-console.log(config.profile[0].avatarURL);
+nothing to see here :)
